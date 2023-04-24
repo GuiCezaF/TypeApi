53e4662e8062601ab18a577fb57eb415188d6568
GuiCezaF <contato.guicezafe@gmail.com>
2023-04-23 23:48:28 -0300

    TestesConsumoAPIExterna

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,22 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+
+  readonly stormGlassAPIParams = 
+  'swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed';
+  readonly stormGlassAPISource = 'noaa'
+
+  constructor(protected request: AxiosStatic) {}
+
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&${lat}&lng=${lng}`, 
+      {
+        headers: {
+            Authorization: 'a026b27e-e245-11ed-a654-0242ac130002-a026b2ec-e245-11ed-a654-0242ac130002'
+        }
+      }
+      );
+  }
+}
+
